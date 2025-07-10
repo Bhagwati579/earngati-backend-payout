c14cfd64c687b46c5db6ba3c969ffe2b7c1bcabb
Bhagwati579 <bhagwatilalpushkarna123@gmail.com>
2025-07-10 17:06:35 +0530

    Delete firebase.js

--- firebase.js
+++
@@ -1,9 +0,0 @@
-const admin = require('firebase-admin');
-const firebaseConfig = JSON.parse(process.env.FIREBASE_CONFIG_JSON);
-
-admin.initializeApp({
-  credential: admin.credential.cert(firebaseConfig),
-  databaseURL: process.env.FIREBASE_DATABASE_URL
-});
-
-module.exports = admin;
